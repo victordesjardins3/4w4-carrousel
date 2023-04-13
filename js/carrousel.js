74b8871619ebeeadeb91512baed414fa191041ce
Victor Desjardins <victordesjardins3@gmail.com>
2023-04-13 11:28:27 -0400

    s11c1 création des img du carrousel

--- js/carrousel.js
+++ js/carrousel.js
@@ -4,12 +4,29 @@
     let boutcarrousel__ouvrir = document.querySelector('.carrousel__ouvrir');
     let carrousel = document.querySelector('.carrousel');
     let carrousel__x = document.querySelector('.carrousel__x');
+    let galerie = document.querySelector(".galerie")
+    let galerie__img = galerie.querySelectorAll("img")
+
+    /**************** OUVRIR LA BOITE ******************/ 
     boutcarrousel__ouvrir.addEventListener('mousedown', function(){
-        carrousel.classList.add('carrousel--ouvrir')
+        carrousel.classList.add('carrousel--ouvrir');
+        ajouter_img_dans_carrousel();
     })
 
+    /**************** FERMER LA BOITE ******************/ 
     carrousel__x.addEventListener('mousedown', function(){
-        carrousel.classList.remove('carrousel--ouvrir')
+        carrousel.classList.remove('carrousel--ouvrir');
     })
+
+    /**
+     * ajouter_img_dans_carrousel
+     * ajouter l'ensemble des images de la galerie dans la boite modale carrousel
+     */
+    function ajouter_img_dans_carrousel(){
+        for (const elm of galerie__img){
+            let img = document.createElement('img');
+            img.setAttribute('src', elm.getAttribute('src'))
+        }
+    }
 
 })()
